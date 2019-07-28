b5191542087342c699b6e845e2c57d54ff29bfc6
zhanghehe <hezhanghe@126.com>
2019-07-29 07:52:49 +0800

    add Android device module

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.media.SoundPool
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,4 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
--- soundmodule/MarkDown.md
+++ soundmodule/MarkDown.md
@@ -1,4 +1,4 @@
 - 游戏中的音效
-1. 即时音效----SoundPool(TODO)
+1. 即时音效----SoundPool(小于7s 的声音资源)
 1. 背景音乐播放技术----MediaPlayer(TODO)
 1. 音量控制-----AudioManager(TODO)
