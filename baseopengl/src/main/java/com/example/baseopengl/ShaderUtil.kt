6209c5ac52349ac2e99c820940f5c38aac5454b6
zhanghehe <hezhanghe@126.com>
2019-08-01 08:17:20 +0800

    add ShaderUtil

---
+++ baseopengl/src/main/java/com/example/baseopengl/ShaderUtil.kt
@@ -0,0 +1,53 @@
+package com.example.baseopengl
+
+import android.opengl.GLES30
+import android.util.Log
+
+object ShaderUtil {
+    fun loadShader(shaderType:Int,source:String):Int{
+        var shader=GLES30.glCreateShader(shaderType)
+        if(shader!=0){
+            GLES30.glShaderSource(shader,source)
+            GLES30.glCompileShader(shader)
+            val compiled=IntArray(1)
+
+            GLES30.glGetShaderiv(shader,GLES30.GL_COMPILE_STATUS,compiled,0)
+
+            if(compiled[0]==0){
+                Log.e("GLES30 loadShader","Could not compile shader $shaderType :")
+                Log.e("GLES30 loadShader",GLES30.glGetShaderInfoLog(shader))
+                GLES30.glDeleteShader(shader)
+                shader=0
+            }
+        }
+        return shader
+    }
+
+    fun createProgram(vertexSource:String,fragmentSource:String):Int{
+        val vertexShader= loadShader(GLES30.GL_VERTEX_SHADER,vertexSource)
+        if(vertexShader==0) {
+            return 0
+        }
+        val pixelShader= loadShader(GLES30.GL_FRAGMENT_SHADER,fragmentSource)
+        if(pixelShader==0){
+            return 0
+        }
+        var program=GLES30.glCreateProgram()
+        if(program!=0) {
+            GLES30.glAttachShader(program,vertexShader)
+            checkGlError("glAttachShader")
+            GLES30.glAttachShader(program,pixelShader)
+            checkGlError("glAttachShader")
+            GLES30.glLinkProgram(program)
+            val linkStatus=IntArray(1)
+            GLES30.glGetProgramiv(program,GLES30.GL_LINK_STATUS,linkStatus,0)
+            if(linkStatus[0]!=GLES30.GL_TRUE){
+                Log.e("GLES30 createProgram","Could not link program")
+                Log.e("GLES30 createProgram",GLES30.glGetProgramInfoLog(program))
+                GLES30.glDeleteProgram(program)
+                program=0
+            }
+        }
+        return program
+    }
+}
